f7a3467e6eb51aa999287c824d57997a550e6c99
KZForrest <tillthend96@gmail.com>
2022-11-28 21:58:04 +0800

    update

---
+++ src/components/Comp1/index.tsx
@@ -0,0 +1,18 @@
+// import "./comp1.scss"
+
+//模块化引入
+
+
+
+
+function Comp() {
+    return (
+        <div className="box">
+            <p>
+                这是comp1内容
+            </p>
+        </div>
+    )
+}
+
+export default Comp
---
+++ src/components/Comp2/index.tsx
@@ -0,0 +1,14 @@
+// import "./comp1.scss"
+
+
+function Comp() {
+    return (
+        <div className="box">
+            <p>
+                这是comp2内容
+            </p>
+        </div>
+    )
+}
+
+export default Comp
